04a7ef41d04f5cf7ff6f12f0dfd535534653aa56
vaistau <vaistaugaityte@gmail.com>
2021-12-03 16:37:13 -0600

    update script

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let divContainer = document.querySelector('#div-container');
 let createDiv;
 
-function repeatDivs() {
-    for (let i = 0; i < 256; i++) {
+function createGrid(numberOfBoxes) {
+    for (let i = 0; i < numberOfBoxes; i++) {
         createDiv = document.createElement('div');
         createDiv.setAttribute('id', 'square');
         divContainer.appendChild(createDiv);
     }
 }
-repeatDivs();
+createGrid(256);
 
