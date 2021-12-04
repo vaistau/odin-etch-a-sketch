687d04b7f7df162e8792a1ecf0758e012e06e6ed
vaistau <vaistaugaityte@gmail.com>
2021-12-03 19:07:39 -0600

    update index and script

--- script.js
+++ script.js
@@ -2,26 +2,43 @@ let divContainer = document.querySelector('#div-container');
 let createDiv;
 let resetButton = document.querySelector('#reset-button');
 
-function createGrid(numberOfBoxes) {
-    for (let i = 0; i < numberOfBoxes * numberOfBoxes; i++) {
+createGrid(16, 16);
+
+let box = document.querySelectorAll('#box');
+addColor()
+
+resetButton.addEventListener('click', function () {
+    box.forEach(box => {
+        box.classList.remove('color');
+    })
+    createGrid(prompt('What should the length and width of your new canvas be?', 16, 'x', 16));
+    addColor();
+})
+
+// FUNCTION DECLARATIONS
+
+function createGrid(length, width) {
+    for (let i = 0; i < length * width; i++) {
         createDiv = document.createElement('div');
         createDiv.setAttribute('id', 'box');
         divContainer.appendChild(createDiv);
     }
 }
 
-createGrid(16);
-
-let box = document.querySelectorAll('#box');
-
-box.forEach(box => {
-    box.addEventListener('mouseenter', function () {
-        box.classList.add('color');
+function addColor() {
+    box = document.querySelectorAll('#box');
+    box.forEach(box => {
+        box.addEventListener('mouseenter', function () {
+            box.classList.add('color');
+        });
     })
-})
+}
 
-resetButton.addEventListener('click', function () {
+function resetCanvas() {
+    box = document.querySelectorAll('#box');
     box.forEach(box => {
         box.classList.remove('color');
     })
-})
+    createGrid(prompt('What should the length and width of your new canvas be?', 16, 'x', 16));
+    addColor();
+}
