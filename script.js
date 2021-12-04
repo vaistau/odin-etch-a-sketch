74ed0ea9626433d01a3f6d8eb28620533e42ad80
vaistau <vaistaugaityte@gmail.com>
2021-12-03 23:15:16 -0600

    update index, script, and style

--- script.js
+++ script.js
@@ -18,9 +18,14 @@ resetButton.addEventListener('click', function () {
 function createGrid(dimension) {
     if (dimension <= 100) {
         for (let i = 0; i < dimension * dimension; i++) {
+            let boxWidth = 480/dimension;
+            let boxHeight = 480/dimension;
+
             createDiv = document.createElement('div');
             createDiv.setAttribute('id', 'box');
+            createDiv.setAttribute('style', `width: ${boxWidth}px; height: ${boxHeight}px;`);
             divContainer.appendChild(createDiv);
+        
         }
     } else {
         alert('Dimensions over 100px are not supported at this time.')
@@ -41,4 +46,9 @@ function addColor() {
 function resetCanvas() {
     box = document.querySelectorAll('#box');
     let removeBox = document.querySelectorAll('#box').forEach(box => box.remove());
+}
+
+function calcBoxSize(number) {
+    let boxWidth = 480/number;
+    let boxHeight = 480/number;
 }
