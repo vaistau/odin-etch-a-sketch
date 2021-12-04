4ebeb8e6807c8c26188fd16b8896aa731510b36c
vaistau <vaistaugaityte@gmail.com>
2021-12-03 18:22:27 -0600

    update index, script, style

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 let divContainer = document.querySelector('#div-container');
 let createDiv;
+let resetButton = document.querySelector('#reset-button');
 
 function createGrid(numberOfBoxes) {
-    for (let i = 0; i < numberOfBoxes; i++) {
+    for (let i = 0; i < numberOfBoxes * numberOfBoxes; i++) {
         createDiv = document.createElement('div');
-        createDiv.setAttribute('id', 'square');
+        createDiv.setAttribute('id', 'box');
         divContainer.appendChild(createDiv);
     }
 }
-createGrid(256);
 
+createGrid(16);
+
+let box = document.querySelectorAll('#box');
+
+box.forEach(box => {
+    box.addEventListener('mouseenter', function () {
+        box.classList.add('color');
+    })
+})
+
+resetButton.addEventListener('click', function () {
+    box.forEach(box => {
+        box.classList.remove('color');
+    })
+})
